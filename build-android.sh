d9ca920a56fae6fa2e5ae1ce9c2efdf17c66df48
devcareer00 <fldevcareer00@gmail.com>
2023-05-17 15:51:23 +0200

    Added missing runtime-link option

--- build-android.sh
+++ build-android.sh
@@ -671,6 +671,7 @@ echo "Building boost for android for $ARCH"
         $cflags                      \
         $cxxflags                    \
         link=static                  \
+        runtime-link=static          \
         threading=multi              \
         --layout=${LAYOUT}           \
         $WITHOUT_LIBRARIES           \
